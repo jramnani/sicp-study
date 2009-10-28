eb0ada690ee2447813dc5935aba5b85e3b105fd8
Jeff Ramnani <jramnani@MacsPower.local>
2009-10-27 21:16:03 -0500

    Chapter 2 - Data Abstraction. Add procedures for rational numbers.

---
+++ clojure/week-4/chap2.0.clj
@@ -0,0 +1,65 @@
+; Procedures that define rational numbers.
+; The book uses 'cons' to construct a 'pair' who's contents are the 
+; numerator and the denominator.
+;
+; Clojure does not have an equivalent data type of 'pair' that can be
+; constructed via 'cons'.  Therefore, I am using (list) to construct
+; a list containing the numerator and the denominator.
+;
+; Since 'cdr'/'rest' returns a list, instead of just the second item 
+; in a pair, use clojure's 'last' to mimic the functionality.
+
+(defn make-rat
+  [n d]
+  (list n d))
+
+(defn numer
+  [x]
+  (first x))
+
+(defn denom
+  [x]
+  (last x))
+
+(defn print-rat
+  [x]
+  (println)
+  (println (first x) "/" (last x)))
+
+(defn add-rat
+  [x y]
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(defn sub-rat
+  [x y]
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(defn mul-rat
+  [x y]
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(defn div-rat
+  [x y]
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(defn equal-rat?
+  [x y]
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+
+(def one-half (make-rat 1 2))
+(def one-third (make-rat 1 3))
+(println one-half)
+(println "Numerator: " (numer one-half))
+(println "Denominator: " (denom one-half))
+(print-rat one-half)
+(print-rat (add-rat one-half one-third)) ; 5 / 6
+(print-rat (mul-rat one-half one-third)) ; 1 / 6
+(print-rat (add-rat one-third one-third)) ; 6 / 9
